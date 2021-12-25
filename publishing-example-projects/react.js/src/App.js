2c9380284227cee1dbf8ff9846515cbdff92ff39
Alfredo Barros <coomcs@gmail.com>
2021-12-25 12:11:44 -0300

    React finished

--- publishing-example-projects/react.js/src/App.js
+++ publishing-example-projects/react.js/src/App.js
@@ -3,25 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    showStockFinder: false
+  }
+
   render() {
+    let stockFinder = null;
+
+    if (this.state.showStockFinder) {
+      stockFinder =  <uc-stock-finder />;
+    }
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <uc-stock-price />
+        {stockFinder}
+        <button onClick={() => this.setState({showStockFinder: true})}>Show Finder</button>
       </div>
-    );
+    )
   }
 }
 
