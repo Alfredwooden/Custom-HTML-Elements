f4ab599b865284417fd83e6cd85c861042e29574
Alfredo Barros <coomcs@gmail.com>
2021-12-25 11:46:50 -0300

    NodeJS finished

--- publishing-example-projects/node/app.js
+++ publishing-example-projects/node/app.js
@@ -5,7 +5,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static('./public'))
+// WEB COMPONENTS SETTINGS
+// app.use(express.static('public'))
+  app.use(express.static('node_modules/web-components-stencil/'))
+// 
 
 app.get('/', (req, res, next) => {
   res.render('starting-page', { message: 'Hello from Node.js' });
